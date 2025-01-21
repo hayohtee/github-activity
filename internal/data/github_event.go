6bf1b5207542b7e330706bad9896d630e1b888c0
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-21 18:56:42 +0100

    Add watchEvent const

--- internal/data/github_event.go
+++ internal/data/github_event.go
@@ -2,6 +2,10 @@ package data
 
 import "time"
 
+const (
+	watchEvent = "WatchEvent"
+)
+
 // repo is a struct containing the id and the name of the github repository.
 type repo struct {
 	ID   int64  `json:"id"`
