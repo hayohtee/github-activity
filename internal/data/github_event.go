6b614f90c1e7a1842c6d313c8b5c8cb90985ab96
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-21 19:52:02 +0100

    Add the implementation for MemberEvent type

--- internal/data/github_event.go
+++ internal/data/github_event.go
@@ -72,6 +72,8 @@ func (g GitHubEvent) String() string {
 		return fmt.Sprintf("User %q %s an issue comment in %s", g.Actor.DisplayLogin, g.Payload.Action, g.Repo.Name)
 	case issuesEvent:
 		return fmt.Sprintf("User %q %s an issue in %s", g.Actor.DisplayLogin, g.Payload.Action, g.Repo.Name)
+	case memberEvent:
+		return fmt.Sprintf("User %q %s an invitation to %s", g.Actor.DisplayLogin, g.Payload.Action, g.Repo.Name)
 	default:
 		return "Unsupported activity"
 	}
