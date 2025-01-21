f4229296beb967983288ded8bc259c8cd4e65957
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-21 19:40:45 +0100

    Add the implementation for CreateEvent type

--- internal/data/github_event.go
+++ internal/data/github_event.go
@@ -60,6 +60,8 @@ func (g GitHubEvent) String() string {
 		return fmt.Sprintf("User %q %s the repository %q", g.Actor.DisplayLogin, g.Payload.Action, g.Repo.Name)
 	case commitCommentEvent:
 		return fmt.Sprintf("User %q %s commit comment in the repository %q", g.Actor.DisplayLogin, g.Payload.Action, g.Repo.Name)
+	case createEvent:
+		return fmt.Sprintf("Created a branch in %s", g.Repo.Name)
 	default:
 		return "Unsupported activity"
 	}
