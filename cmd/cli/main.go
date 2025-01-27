aad35f55a85dbd71fa31db603168f936f828d65d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-27 18:18:23 +0100

    Add fetchGithubEvents function

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
+
+	"github.com/hayohtee/github-activity/internal/data"
 )
 
 func main() {
@@ -10,4 +15,47 @@ func main() {
 		fmt.Fprintln(os.Stderr, "no argument provided")
 		os.Exit(1)
 	}
+
+	username := os.Args[1]
+}
+
+// fetchGithubEvents fetches the GitHub events for a specified user.
+//
+// Parameters:
+//   - username: The GitHub username for which to fetch events.
+//
+// Returns:
+//   - A slice of GitHubEvent structs containing the fetched events.
+//   - An error if the request fails or the response cannot be decoded.
+//
+// The function makes an HTTP GET request to the GitHub API to retrieve the
+// events for the specified user. It sets the appropriate headers for the
+// request and decodes the JSON response into a slice of GitHubEvent structs.
+func fetchGithubEvents(username string) ([]data.GitHubEvent, error) {
+	var githubEvents []data.GitHubEvent
+
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	apiUrl := fmt.Sprintf("https://api.github.com/users/%s/events", username)
+
+	req, err := http.NewRequest(http.MethodGet, apiUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&githubEvents); err != nil {
+		return nil, err
+	}
+
+	return githubEvents, nil
 }
